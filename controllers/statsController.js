e00273036b3e667a3475ad7d983f2ae8d25f2ff0
Venkata Mahesh Polumuri <venkat2552004@gmail.com>
2024-10-11 16:16:59 +0530

    Fix improper try block implementation

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -1,28 +1,28 @@
-const Crypto = require('../models/crypto');
+const Crypto = require("../models/crypto");
 
-const fetchKoinStats = async(req, res) => {
-    const coin = req.query.coin;
+const fetchKoinStats = async (req, res) => {
+	const coin = req.query.coin;
 
-    if(!coin){
-        throw new Error("Coin not found in the request");
-    }
-
-    if (!["bitcoin", "matic-network", "ethereum"].includes(coin.toLowerCase())) {
-		throw new Error("Invalid Coin, only bitcoin, matic-network and ethereum are allowed");
+	if (!coin) {
+		throw new Error("Coin not found in the request");
 	}
 
-    try {
-        const stats =await Crypto.findOne({name : coin});
-    }
-    catch(err){
-        throw new Error(`Error occured while fetching stats about ${coin}`)
-    }
-    
-    res.json({
+	if (!["bitcoin", "matic-network", "ethereum"].includes(coin.toLowerCase())) {
+		throw new Error(
+			"Invalid Coin, only bitcoin, matic-network and ethereum are allowed"
+		);
+	}
+
+	try {
+		const stats = await Crypto.findOne({ name: coin });
+		res.json({
 			price: stats.price,
 			marketCap: stats.marketCap,
 			"24hChange": stats._24hChange,
-	});
-}
+		});
+	} catch (err) {
+		throw new Error(`Error occured while fetching stats about ${coin}`);
+	}
+};
 
 module.exports = fetchKoinStats;
--- routes/router.js
+++ routes/router.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const fetchKoinStats = require("../controllers/statsController");
 const calculateDeviation = require("../controllers/deviationController");
-
-
 const asyncHanlder = require('express-async-handler');
 const router = express.Router();
 
-router.route("/stats").get(asyncHanlder(fetchKoinStats));
+router.route("/stats").get(fetchKoinStats);
 router.route("/deviation").get(asyncHanlder(calculateDeviation));
+router.all("*", () => {
+    throw new Error(
+			"Please check the URL or visit https://github.com/Venkat2552004/KoinX-KoinMonitor/blob/main/README.md"
+		);
+});
 
 module.exports = router;
